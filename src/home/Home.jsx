4836e14f9bfea486a9afd00f00e64112e8c3769d
Rachel Kwon <jk2353@cornell.edu>
2019-04-15 19:47:30 -0400

    Rachel - dropdown navbar

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,11 +5,39 @@ import MainImage from './image/MainImage.jpeg';
 import Timeline from './image/Timeline.png';
 import threeimages from './image/threeimages.png';
 import threesteps from './image/threesteps.png';
+import gray from './image/gray.png';
 
 import './Home.css';
 
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showAbout: false,
+    }
+
+    this.showAbout = this.showAbout.bind(this);
+    this.closeAbout = this.closeAbout.bind(this);
+  }
+
+  showAbout(event) {
+    event.preventDefault();
+
+    this.setState({ showAbout: true }, () => {
+      document.addEventListener('click', this.closeAbout);
+    });
+  }
+
+  closeAbout(event) {
+    if (!this.dropdownAbout.contains(event.target)) {
+    this.setState({ showAbout: false}, () => {
+      document.removeEventListener('click', this.closeAbout);
+    });
+  }
+}
+
     state = {
         name: 'HOME',
     }
@@ -18,17 +46,48 @@ class Home extends Component {
         const { name } = this.state;
         return (
             <div className="App">
-                <div class = "nav">
-                    <div class = "navButton">
-                        <a href = "About.jsx" class = "navAbout">ABOUT</a>
-                        <a href = "" class = "navProjects">PROJECTS</a>
-                        <a href = "" class = "navKor">한국어</a>
-                        <a href = "" class = "navShop">SHOP</a>
-                        <img src = {tiptoe} class ="mainlogo" alt = "logo"/>
-                    </div>
+              <div class ="dropdown">
+                <button class = "dropdown_b" onClick={this.showAbout}>ABOUT</button>
+            </div>
+
+              <div class = "nav">
+                  <div class = "navbutton">
+                      <a href = "" class = "navProjects">PROJECTS</a>
+                      <a href = "" class = "navKor">한국어</a>
+                      <a href = "" class = "navShop">SHOP</a>
+                      <img src = {tiptoe} class ="mainlogo" alt = "logo"/>
+                  </div>
                 </div>
 
-                <div class = "imageText">
+                {
+                  this.state.showAbout
+                    ? (
+                      <div className = "dropdown" ref = {(element) => {
+                        this.dropdownAbout = element;
+                      }}
+                      >
+                        <div class = "container">
+                          <div class = "buttonbox">
+                            <button class = "button"> OUR STORY </button>
+                            <button class = "button"> OUR TEAM </button>
+                            <button class = "button"> PARTNERS/PRESS </button>
+                            <button class = "button"> JOIN US </button>
+                          </div>
+                          <div class = "imagebox1">
+                            <img src = {gray} class = "navbarimage1"/>
+                            <div class = "imageboxtext">OUR STORY</div>
+                          </div>
+                          <div class = "imagebox2">
+                            <img src = {gray} class = "navbarimage2"/>
+                            <div class = "imageboxtext">OUR STORY</div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                    : ( null )
+                  }
+
+              <div class = "imageText">
                     <img src = {MainImage} class = "mainImage" alt = ""/>
                     <h1 class = "mainImageText2">spring ‘19 new release</h1>
                     <h1 class = "mainImageText1">spring ‘19 new release</h1>
@@ -58,7 +117,9 @@ class Home extends Component {
                 <p class = "tiptoe">TIPTOE& 2019</p>
 
 
+
             </div>
+
 
 
         );
