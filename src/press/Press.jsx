bcf7b4005a77d0b523580b8a42f50db9899f9382
Jae Mok Yoon <jy392@cornell.edu>
2019-05-06 18:09:57 -0400

    remove commments

--- src/press/Press.jsx
+++ src/press/Press.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../navbar/Navbar.jsx';
-import tiptoe from './image/tiptoe.png';
-import gray from './image/gray.png';
 import Footer from '../footer/footer';
 import './Press.css';
-import BusinessReview from './image/cornellbusinessreview.png';
-import Glogo from './image/Jungle.png';
-import Koreadaily1 from './image/koreadaily.png';
-import Koreadaily2 from './image/koreadailylogo.jpg';
-import Koreadaily3 from './image/koreadailylogosq.png';
-import Bluetree from './image/bluetree.png';
 import PressImage from './PressImage';
 
 
