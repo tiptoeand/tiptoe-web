94c7418f5f722e6d5665c4457d67834b97b49bb3
jy392 <jy392@cornell.edu>
2019-12-09 05:24:14 -0500

    Removed horizontal scrolling

--- src/navbar/NavbarMobile.jsx
+++ src/navbar/NavbarMobile.jsx
@@ -44,12 +44,12 @@ export default class NavbarMobile extends Component {
           <input class="menu-btn" type="checkbox" id="menu-btn" />
           <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
           <ul class="menu">
-            <li><a href="/ourstory">OUR STORY</a></li>
-            <li><a href="/ourteam">OUR TEAM</a></li>
+            <li><a href="/ourstory">ABOUT</a></li>
+            {/*<li><a href="/ourteam">OUR TEAM</a></li>*/}
             <li><a href="/projects">PROJECTS</a></li>
-            <li><a href="/press">PARTNERS/PRESS</a></li>
-            <li><a href="/contact">JOIN US</a></li>
-            <li><a href="/shop">SHOP</a></li>
+            <li><a href="/press">PRESS</a></li>
+            <li><a href="/contact">CONTACT</a></li>
+            {/*<li><a href="/shop">SHOP</a></li>*/}
           </ul>
         </header>
 
