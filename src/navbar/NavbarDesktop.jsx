b88822edf41ba8e331bcb555464ddbe2bb9ef5f6
jy392 <jy392@cornell.edu>
2020-11-28 15:53:42 -0500

    temp

--- src/navbar/NavbarDesktop.jsx
+++ src/navbar/NavbarDesktop.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import tiptoe from "../image/tiptoe.png";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
 import "./NavbarDesktop.css";
 
 function NavbarDesktop() {
   return (
-    <div class="navbardesktop pt-1">
-      <nav class="desktopnavbar navbar navbar-light justify-content-between">
-        <div></div>
-        <a class="navbar-brand" href="/ourstory">
-          ABOUT
-        </a>
-        <a class="navbar-brand" href="/projects">
-          PROJECTS
-        </a>
-        <a class="navbar-brand" href="/">
-          <img src={tiptoe} class="desktoplogoimage" alt="TIPTOE& Logo" />
-        </a>
-        <a class="navbar-brand" href="/press">
-          PRESS
-        </a>
-        <a class="navbar-brand" href="/contact">
-          CONTACT
-        </a>
-        <div></div>
-      </nav>
-    </div>
+    // <div class="navbardesktop pt-1">
+    //   <nav class="desktopnavbar navbar navbar-light justify-content-between">
+    //     <div></div>
+    //     <a class="navbar-brand" href="/ourstory">
+    //       ABOUT
+    //     </a>
+    //     <a class="navbar-brand" href="/projects">
+    //       PROJECTS
+    //     </a>
+    //     <a class="navbar-brand" href="/">
+    //       <img src={tiptoe} class="desktoplogoimage" alt="TIPTOE& Logo" />
+    //     </a>
+    //     <a class="navbar-brand" href="/press">
+    //       PRESS
+    //     </a>
+    //     <a class="navbar-brand" href="/contact">
+    //       CONTACT
+    //     </a>
+    //     <div></div>
+    //   </nav>
+    // </div>
+    <div></div>
   );
 }
 export { NavbarDesktop };
